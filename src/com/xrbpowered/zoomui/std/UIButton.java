b0a377968c7e508808a5a3b9d54ffe7f067e5ed8
Ashur Rafiev <7903175+AshurAxelR@users.noreply.github.com>
2022-09-10 22:26:40 +0100

    Paint disabled button

--- src/com/xrbpowered/zoomui/std/UIButton.java
+++ src/com/xrbpowered/zoomui/std/UIButton.java
@@ -13,8 +13,10 @@ public class UIButton extends UIButtonBase {
 	public static Font font = new Font("Tahoma", Font.PLAIN, GraphAssist.ptToPixels(9f));
 	
 	public static Color colorDown = new Color(0xd4d4d4);
+	public static Color colorDisabled = new Color(0xe7e7e7);
 	public static Color colorBorder = new Color(0x888888);
 	public static Color colorText = Color.BLACK;
+	public static Color colorTextDisabled = new Color(0x888888);
 	public static Color colorGradTop = new Color(0xeeeeee);
 	public static Color colorGradBottom = new Color(0xcccccc);
 
@@ -31,10 +33,10 @@ public class UIButton extends UIButtonBase {
 	
 	@Override
 	public void paint(GraphAssist g) {
-		g.setPaint(down ? colorDown : new GradientPaint(0, 0, colorGradTop, 0, getHeight(), colorGradBottom));
+		g.setPaint(isEnabled() ? (down ? colorDown : new GradientPaint(0, 0, colorGradTop, 0, getHeight(), colorGradBottom)) : colorDisabled);
 		g.fill(this);
 		g.border(this, hover ? colorText : colorBorder);
-		g.setColor(colorText);
+		g.setColor(isEnabled() ? colorText : colorTextDisabled);
 		g.setFont(font);
 		g.drawString(label, getWidth()/2f, getHeight()/2f, GraphAssist.CENTER, GraphAssist.CENTER);
 	}
